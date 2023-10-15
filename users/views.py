c9ade992119e5033bd04add64d1566c8d99c434d
Eldiyar1 <mc.oks1ne@gmail.com>
2023-10-16 03:05:55 +0600

    comment cbv

--- users/views.py
+++ users/views.py
@@ -62,54 +62,54 @@ def logout(request):
         return Response({'message': 'User is already logged out'}, status=status.HTTP_200_OK)
 
 
-class RegistrationAPIView(APIView):
-    serializer_class = UserRegisterSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = User.objects.create_user(**serializer.validated_data, is_active=False)
-        confirmation = UserConfirmation.objects.create(user=user, code=random.randint(100000, 999999))
-        return Response({'status': 'User registered', 'code': confirmation.code, 'data': serializer.data},
-                        status=status.HTTP_201_CREATED)
-
-
-class ConfirmUserAPIView(APIView):
-    serializer_class = UserConfirmationSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        code = serializer.validated_data.get('code')
-        confirmation = get_object_or_404(UserConfirmation, code=code)
-        user = confirmation.user
-        user.is_active = True
-        user.save()
-        confirmation.delete()
-        return Response({'status': 'User activated'}, status=status.HTTP_200_OK)
-
-
-class AuthorizationAPIView(APIView):
-    serializer_class = UserLoginSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = authenticate(**serializer.validated_data)
-        login(request, user)
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key})
-        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LogoutAPIView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request):
-        try:
-            token = Token.objects.get(user=request.user)
-            token.delete()
-            return Response({'message': 'User logged out'}, status=status.HTTP_200_OK)
-        except Token.DoesNotExist:
-            return Response({'message': 'User is already logged out'}, status=status.HTTP_200_OK)
+# class RegistrationAPIView(APIView):
+#     serializer_class = UserRegisterSerializer
+#
+#     def post(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = User.objects.create_user(**serializer.validated_data, is_active=False)
+#         confirmation = UserConfirmation.objects.create(user=user, code=random.randint(100000, 999999))
+#         return Response({'status': 'User registered', 'code': confirmation.code, 'data': serializer.data},
+#                         status=status.HTTP_201_CREATED)
+#
+#
+# class ConfirmUserAPIView(APIView):
+#     serializer_class = UserConfirmationSerializer
+#
+#     def post(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         code = serializer.validated_data.get('code')
+#         confirmation = get_object_or_404(UserConfirmation, code=code)
+#         user = confirmation.user
+#         user.is_active = True
+#         user.save()
+#         confirmation.delete()
+#         return Response({'status': 'User activated'}, status=status.HTTP_200_OK)
+#
+#
+# class AuthorizationAPIView(APIView):
+#     serializer_class = UserLoginSerializer
+#
+#     def post(self, request):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = authenticate(**serializer.validated_data)
+#         login(request, user)
+#         if user:
+#             token, created = Token.objects.get_or_create(user=user)
+#             return Response({'token': token.key})
+#         return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class LogoutAPIView(APIView):
+#     permission_classes = (IsAuthenticated,)
+#
+#     def post(self, request):
+#         try:
+#             token = Token.objects.get(user=request.user)
+#             token.delete()
+#             return Response({'message': 'User logged out'}, status=status.HTTP_200_OK)
+#         except Token.DoesNotExist:
+#             return Response({'message': 'User is already logged out'}, status=status.HTTP_200_OK)
